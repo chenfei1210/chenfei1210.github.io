f67386207488489a95c21acd9bc333847cf78f30
ChenFei <cf273126001@gmail.com>
2025-07-09 11:32:36 +0900

    🔧 build: update build
    update build settings

---
+++ character_conversion.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+find . -type f -name "*.md" | while read -r file; do
+  echo "处理文件：$file"
+
+  sed -i '' \
+  -e 's|（|\(|g' \
+  -e 's|）|\)|g' \
+  -e 's|，|, |g' \
+  -e 's|：|: |g' \
+  $file
+
+done
